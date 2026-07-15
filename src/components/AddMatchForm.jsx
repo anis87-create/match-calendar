d7245b880c1c62e84a4e937b2d68daeac7238bc1
anis87-create <aniscode87@gmail.com>
2026-07-15 11:31:54 +0100

    Remove "Autre" option from team select

--- src/components/AddMatchForm.jsx
+++ src/components/AddMatchForm.jsx
@@ -452,7 +452,6 @@ export default function AddMatchForm() {
               return <option key={id} value={id}>{team.name}</option>;
             })}
             <option value="important">⭐ Grand match</option>
-            <option value="autre">📋 Autre</option>
           </select>
         </div>
       </div>
